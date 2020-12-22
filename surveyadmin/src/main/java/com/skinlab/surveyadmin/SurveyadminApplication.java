34bbc06bc8177a396f1a55e49798123d758fb168
hangpy <leehangbok2009@gmail.com>
2020-12-23 01:46:42 +0900

    Set rough environments

---
+++ surveyadmin/src/main/java/com/skinlab/surveyadmin/SurveyadminApplication.java
@@ -0,0 +1,13 @@
+package com.skinlab.surveyadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyadminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyadminApplication.class, args);
+	}
+
+}
